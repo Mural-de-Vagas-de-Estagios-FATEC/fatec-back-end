2a1624fc783837de8fb5ff0fb4af7017bd8e9ced
Pedro H G Cavalcanti <77908211+ClaitonsAce@users.noreply.github.com>
2021-09-11 10:55:12 -0300

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,8 @@ if(isset($_POST['confirmar'])){//se o confirmar for pressionado
     $sql = "SELECT * FROM usuários WHERE EMAIL='$email'";
     $query = mysqli_query($mysqli,$sql);/*realiza o comando SQL, a variavel $mysqli
     está no arquivo do banco de dados*/
+    $sqlEmpresa = "SELECT * FROM EMPRESA WHERE EMAIL='$email'";
+    $queryEmpresa = mysqli_query($mysqli,$sqlEmpresa);
     if(mysqli_num_rows($query)){//se tiver alguma linha com a pesquisa efetuada retorna verdadeiro
         $infoUsuario = mysqli_fetch_row($query);
         $senhaBD = $infoUsuario[2];
@@ -19,6 +21,8 @@ if(isset($_POST['confirmar'])){//se o confirmar for pressionado
             $_SESSION['email'] = $infoUsuario[1];
             $_SESSION['curso'] = $infoUsuario[3];
             $_SESSION['semestre'] = $infoUsuario[4];
+            $_SESSION['situacao'] = $infoUsuario[5];
+            $_SESSION['id']= $infoUsuario[6];
             header("Location: home.php");
             exit(); 
             }
@@ -26,6 +30,24 @@ if(isset($_POST['confirmar'])){//se o confirmar for pressionado
             echo 'Senha Incorreta';
         }
 
+    }
+    elseif (mysqli_num_rows($queryEmpresa)) {
+        $infoEmpresa = mysqli_fetch_row($queryEmpresa);
+        $senhaBD = $infoEmpresa[2];
+        //if(password_verify($senha, $senhaBD)){
+        if($senha == $senhaBD){
+            $_SESSION['nome'] = $infoEmpresa[0];
+            $_SESSION['email'] = $infoEmpresa[1];
+            $_SESSION['telefone'] = $infoEmpresa[3];
+            $_SESSION['celular'] = $infoEmpresa[4];
+            $_SESSION['cidade'] = $infoEmpresa[5];
+            $_SESSION['cnpj'] = $infoEmpresa[6];
+            $_SESSION['id'] = $infoEmpresa[7];
+            $_SESSION['situacao'] = "Empresa";
+            header("Location: home.php");
+            exit();
+        }
+        //}
     }
     else {
         echo 'Email Incorreto';
@@ -57,14 +79,14 @@ if(isset($_POST['confirmar'])){//se o confirmar for pressionado
                     <img src="./public/assets/logo-fatec.png" alt="logo" width="120">
                 </div>
                 <h3 class="login-h3">Login</h3>
-                <form id="formLogin" action="index.php" method="Post" enctype="multipart/form-data">
+                <form id="formLogin" action="index.php" method="post" enctype="multipart/form-data">
                     <input type="email" name="login" id="login-input" placeholder="example@gmail.com" class="input">
                     <input type="password" name="password" id="password-input" placeholder="••••••••••" class="input">
                     <div class="buttons-forms">
                         <button type="submit" name="confirmar" class="button">ENTRAR</button>
-                        <a href="#">Esqueci minha Senha</a>
+                        <a href="recuperar_senha.php">Esqueci minha Senha</a>
                     </div>
-                    <h4>Não possui login? <a href="#">Faça já seu cadastro</a> </h4>
+                    <h4>Não possui login? <a href="cadastros.html">Faça já seu cadastro</a> </h4>
                     
                 </form>
             </div>
@@ -83,40 +105,34 @@ if(isset($_POST['confirmar'])){//se o confirmar for pressionado
                 <details>
                     <summary> Vagas de estágio para alunos da Fatec Itaquá </summary>
                     <p>
-                    Adipisicing cupidatat magna culpa id. Do nostrud ea consectetur officia labore adipisicing dolore adipisicing amet irure. Aliquip cillum esse adipisicing deserunt. Non duis elit tempor in adipisicing est reprehenderit Lorem adipisicing.
-                    Adipisicing cupidatat magna culpa id. Do nostrud ea consectetur officia labore adipisicing dolore adipisicing amet irure. Aliquip cillum esse adipisicing deserunt. Non duis elit tempor in adipisicing est reprehenderit Lorem adipisicing.
-                    Adipisicing cupidatat magna culpa id. Do nostrud ea consectetur officia labore adipisicing dolore adipisicing amet irure. Aliquip cillum esse adipisicing deserunt. Non duis elit tempor in adipisicing est reprehenderit Lorem adipisicing.
+                    Na plataforma, como função principal é possível usar um mural de vagas para estágios onde permite que qualquer usuário possa visualizar as vagas com seus dados e requisitos assim permitindo aos alunos que identifiquem vagas com os seus perfis e melhor divulgação das vagas.
                     </p>
                  </details> 
     
                  <details>
                     <summary> Divulgue vagas da sua empresa em nossa plataforma </summary>
                     <p>
-                    Adipisicing cupidatat magna culpa id. Do nostrud ea consectetur officia labore adipisicing dolore adipisicing amet irure. Aliquip cillum esse adipisicing deserunt. Non duis elit tempor in adipisicing est reprehenderit Lorem adipisicing.
-                    Adipisicing cupidatat magna culpa id. Do nostrud ea consectetur officia labore adipisicing dolore adipisicing amet irure. Aliquip cillum esse adipisicing deserunt. Non duis elit tempor in adipisicing est reprehenderit Lorem adipisicing.
-                    Adipisicing cupidatat magna culpa id. Do nostrud ea consectetur officia labore adipisicing dolore adipisicing amet irure. Aliquip cillum esse adipisicing deserunt. Non duis elit tempor in adipisicing est reprehenderit Lorem adipisicing.
+                    Para empresas  interessadas em anunciar vagas temos a área de solicitação de cadastro de vaga , é recomendado que a empresa faça o cadastro da conta e entre na página de solicitação de divulgação de vagas para que elas possam aparecer no mural de vagas, é possível também solicitar para a instituição de ensino através de telefone e a própria instituição cadastrará a vaga.
                     </p>
                  </details> 
     
                  <details>
                     <summary> Network entre alunos e ex-alunos da Fatec Itaquá </summary>
                     <p>
-                    Adipisicing cupidatat magna culpa id. Do nostrud ea consectetur officia labore adipisicing dolore adipisicing amet irure. Aliquip cillum esse adipisicing deserunt. Non duis elit tempor in adipisicing est reprehenderit Lorem adipisicing.
-                    Adipisicing cupidatat magna culpa id. Do nostrud ea consectetur officia labore adipisicing dolore adipisicing amet irure. Aliquip cillum esse adipisicing deserunt. Non duis elit tempor in adipisicing est reprehenderit Lorem adipisicing.
-                    Adipisicing cupidatat magna culpa id. Do nostrud ea consectetur officia labore adipisicing dolore adipisicing amet irure. Aliquip cillum esse adipisicing deserunt. Non duis elit tempor in adipisicing est reprehenderit Lorem adipisicing.
+                    Para uma melhor interação e compartilhamento de experiencias entre alunos interessados em estágios e os egressos, existe o fórum que permite que a comunicação seja publica e proporciona que pessoas com experiencia na área possam dar conselhos ou apresentar novas oportunidade de crescimento na área.
                     </p>
                  </details>
             </div> 
             <div class="center">
-                <a href="#" class="button-dark">CADASTRE-SE</a>
+                <a href="cadastros.html" class="button-dark">CADASTRE-SE</a>
             </div>
             <footer>
                 <p>© Todos os direitos Reservados - <a href="#">Fatec Itaquaquecetuba</a></p>
                 <div class="social-icon">
-                    <a href="#"><i class="fab fa-instagram"></i></a>
-                    <a href="#"><i class="fab fa-linkedin"></i></a>
-                    <a href="#"><i class="fa fa-youtube"></i></a>
-                    <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="https://www.instagram.com/fatecitaquaquecetuba/"><i class="fab fa-instagram"></i></a>
+                        <a href="https://www.linkedin.com/company/fatec-itaquaquecetuba/"><i class="fab fa-linkedin"></i></a>
+                        <a href="https://www.youtube.com/channel/UC6LUrLB2BOaw_J_LOHyaisA"><i class="fa fa-youtube"></i></a>
+                        <a href="https://twitter.com/FatecItaqua"><i class="fa fa-twitter"></i></a>
                 </div>
             </footer>
         </div>
