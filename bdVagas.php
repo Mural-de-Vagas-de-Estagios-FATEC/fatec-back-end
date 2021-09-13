f6464f852030a0f4d58e8dfd4dffede5d491a4bc
Pedro H G Cavalcanti <77908211+ClaitonsAce@users.noreply.github.com>
2021-09-12 22:16:43 -0300

    Update bdVagas.php

--- bdVagas.php
+++ bdVagas.php
@@ -1,18 +1,17 @@
 <?php
 
-$sqlVagas = "SELECT * FROM VAGAS";
+$sqlVagas = "SELECT * FROM vagas";
 $queryVagas = mysqli_query($mysqli, $sqlVagas);
-
 if(mysqli_num_rows($queryVagas) > 0) {
-	$sqlId = "SELECT ID_VAGA FROM VAGAS";
-	$queryId = mysqli_query($mysqli, $sqlId);
-	$infoId = mysqli_fetch_all($queryId);
+    $sqlId = "SELECT ID_VAGA FROM vagas";
+    $queryId = mysqli_query($mysqli, $sqlId);
+    $infoId = mysqli_fetch_all($queryId);
 
-	$j = sizeof($infoId) - 1;
-	for ($i=0; $i <= $j ; $j--) {
-        $sqlPubli = "SELECT EMPRESA.NOME, EMPRESA.EMAIL, EMPRESA.TELEFONE, EMPRESA.CELULAR, EMPRESA.CIDADE,
-        VAGAS.NOME_VAGA, VAGAS.REGIME, VAGAS.CURSO, VAGAS.QUANTIDADE, VAGAS.PERFIL FROM EMPRESA INNER JOIN 
-        VAGAS ON EMPRESA.ID_EMPRESA = VAGAS.AUTOR WHERE VAGAS.ID_VAGA=" . $infoId[$j][0];
+    $j = sizeof($infoId) - 1;
+    for ($i=0; $i <= $j ; $j--) {
+        $sqlPubli = "SELECT empresa.NOME, empresa.EMAIL, empresa.TELEFONE, empresa.CELULAR, empresa.CIDADE, empresa.IMAGEM,
+        vagas.NOME_VAGA, vagas.REGIME, vagas.CURSO, vagas.QUANTIDADE, vagas.PERFIL FROM empresa INNER JOIN 
+        vagas ON empresa.ID_EMPRESA = vagas.AUTOR WHERE vagas.ID_VAGA=" . $infoId[$j][0];
         $queryPubli = mysqli_query($mysqli,$sqlPubli);
         $infoVagas = mysqli_fetch_row($queryPubli);
 
@@ -21,7 +20,7 @@ if(mysqli_num_rows($queryVagas) > 0) {
 <div class="container-vaga">
                             <div class="dados-da-vaga">
                                 <div class="imagem-empresa">
-                                    <img src="./public/assets/imagem-teste.jpg" alt="imagem da empresa">
+                                    <img src="<?= $infoVagas[5]?>" alt="imagem da empresa" width="100px" height="100px">
                                 </div>
                                 <div class="informacao-vaga">
                                     <h3><?= $infoVagas[0]?></h3>
@@ -29,16 +28,16 @@ if(mysqli_num_rows($queryVagas) > 0) {
                                     <h4>Tel:<?= $infoVagas[2]?> / Cel:<?= $infoVagas[3]?></h4>
                                 </div>
                             </div>
-                            <div class="nome-da-vaga center"><?= $infoVagas[5]?></div>
+                            <div class="nome-da-vaga center"><?= $infoVagas[6]?></div>
                             <div class="requisitos-vaga">
                                 <h4>Perfil da vaga:</h4>
-                                <p><?= $infoVagas[9]?> </p>
+                                <p><?= $infoVagas[10]?> </p>
                             </div>
                     
                             <div class="info-adicionais-sobre-vaga center">
                                 <div>
                                     <i class="fas fa-briefcase"></i>
-                                    <?= $infoVagas[7] . ' / ' . $infoVagas[8] . ' Vagas'?>
+                                    <?= $infoVagas[8] . ' / ' . $infoVagas[9] . ' Vagas'?>
                                 </div>
                                 <div>
                                     <i class="fas fa-map-marker-alt"></i>
@@ -46,11 +45,11 @@ if(mysqli_num_rows($queryVagas) > 0) {
                                 </div>
                                 <div>
                                     <i class="far fa-clock"></i>
-                                    <?= $infoVagas[6]?>
+                                    <?= $infoVagas[7]?>
                                 </div>
                             </div>
                         </div>
 <?php
-	}
+    }
 }
 ?>
